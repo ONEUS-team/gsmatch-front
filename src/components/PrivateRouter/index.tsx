a4185a4d220c8ee864c371e1e2e5a09d37b2f698
신희성 <vvwv3092@gmail.com>
2023-08-23 17:51:28 +0900

    Add PrivateRoutering

--- src/components/PrivateRouter/index.tsx
+++ src/components/PrivateRouter/index.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = ({ component }: any) => {
+const PrivateRoute = ({ element }: any) => {
   const [isLoggedin, setIsLoggedin] = useState(false);
 
-  return isLoggedin ? component : <Navigate to="/login"></Navigate>;
+  return isLoggedin ? element : <Navigate to="/login"></Navigate>;
 };
 
 export default PrivateRoute;
