fc66be570e8f68b184764022511ff9b2ac69e76d
방가온 <128475660+gaoooon@users.noreply.github.com>
2023-08-31 23:04:40 +0900

    Revert "Feature/loginpage"

--- src/Assets/svg/index.ts
+++ src/Assets/svg/index.ts
@@ -1,5 +1,2 @@
 export { default as ArrowHeadIcon } from "./ArrowHeadIcon";
 export { default as MainPageLogo } from "./MainPageLogo";
-export { default as HeaderLogo } from "./HeaderLogo";
-export { default as PasswordHideIcon } from "./PasswordHideIcon";
-export { default as PasswordShowIcon } from "./PasswordShowIcon";
