a5a53a1d6f964268720b09e614809e6e5c03dd25
gaoooon <gaon9749@gmail.com>
2023-09-06 16:47:39 +0900

    Add GoBackIcon

--- src/Assets/svg/index.ts
+++ src/Assets/svg/index.ts
@@ -3,3 +3,4 @@ export { default as MainPageLogo } from "./MainPageLogo";
 export { default as HeaderLogo } from "./HeaderLogo";
 export { default as PasswordHideIcon } from "./PasswordHideIcon";
 export { default as PasswordShowIcon } from "./PasswordShowIcon";
+export { default as GoBackIcon } from "./GoBack";
