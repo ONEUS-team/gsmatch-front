a8b91c3d98c7b552bced04b9af5a24d845a580b9
gaoooon <gaon9749@gmail.com>
2023-09-06 20:38:53 +0900

    Update index.ts

--- src/Assets/svg/index.ts
+++ src/Assets/svg/index.ts
@@ -3,4 +3,4 @@ export { default as MainPageLogo } from "./MainPageLogo";
 export { default as HeaderLogo } from "./HeaderLogo";
 export { default as PasswordHideIcon } from "./PasswordHideIcon";
 export { default as PasswordShowIcon } from "./PasswordShowIcon";
-export { default as GoBackIcon } from "./GoBack";
+export { default as GoBackIcon } from "./GoBackIcon";
