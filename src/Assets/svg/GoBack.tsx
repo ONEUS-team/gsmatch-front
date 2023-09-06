b7a8b3d7dbbb950445d2e1743f793612d6c20780
gaoooon <gaon9749@gmail.com>
2023-09-06 16:45:18 +0900

    Add GoBackIcon

---
+++ src/Assets/svg/GoBack.tsx
@@ -0,0 +1,18 @@
+const GoBack = () => (
+  <svg
+    width="16"
+    height="28"
+    viewBox="0 0 16 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M14 2L2.70711 13.2929C2.31658 13.6834 2.31658 14.3166 2.70711 14.7071L14 26"
+      stroke="#EEEEEE"
+      stroke-width="3"
+      stroke-linecap="round"
+    />
+  </svg>
+);
+
+export default GoBack;
