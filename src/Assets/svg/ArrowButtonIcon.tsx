3ac371e1ce9d5f85da689c4d453676d2ae9c2960
gaoooon <gaon9749@gmail.com>
2023-09-14 19:59:07 +0900

    Add Icons

---
+++ src/Assets/svg/ArrowButtonIcon.tsx
@@ -0,0 +1,18 @@
+const ArrowButtonIcon = () => (
+  <svg
+    width="9"
+    height="16"
+    viewBox="0 0 9 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.5 2L6.79289 7.29289C7.18342 7.68342 7.18342 8.31658 6.79289 8.70711L1.5 14"
+      stroke="#292424"
+      strokeWidth="3"
+      strokeLinecap="round"
+    />
+  </svg>
+);
+
+export default ArrowButtonIcon;
--- src/Assets/svg/index.ts
+++ src/Assets/svg/index.ts
@@ -3,3 +3,6 @@ export { default as MainPageLogo } from "./MainPageLogo";
 export { default as HeaderLogo } from "./HeaderLogo";
 export { default as PasswordHideIcon } from "./PasswordHideIcon";
 export { default as PasswordShowIcon } from "./PasswordShowIcon";
+export { default as GenreRequestIcon } from "./GenreRequestIcon";
+export { default as MajorRequestIcon } from "./MajorRequestIcon";
+export { default as ArrowButtonIcon } from "./ArrowButtonIcon";
