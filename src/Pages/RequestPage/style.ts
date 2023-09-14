c8d3f9f6441068d7b69aec6ac63f22b4203041e1
gaoooon <gaon9749@gmail.com>
2023-09-14 17:24:34 +0900

    Add style.ts

---
+++ src/Pages/RequestPage/style.ts
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #111;
+  height: 100vh;
+`;
+
+export const TextItem = styled.h1`
+  margin-top: 13rem;
+  color: #eeeeee;
+  font-weight: 700;
+  font-size: 2rem;
+`;
+
+export const SubTextItem = styled.p`
+  margin-top: 1rem;
+  color: #777777;
+  font-weight: 500;
+  font-size: 1rem;
+`;
+
+export const SelectForm = styled.form`
+  display: flex;
+`;
+
+export const SelectItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #777777;
+  align-items: center;
+`;
+
+export const Icon = styled.img`
+  width: 160px;
+  height: 160px;
+`;
+
+export const SelectTitle = styled.h1``;
+
+export const SelectInfo = styled.p``;
