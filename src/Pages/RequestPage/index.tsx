13db60fdd9188a9a67607e39e154a5475ccf85de
gaoooon <gaon9749@gmail.com>
2023-09-14 17:24:48 +0900

    Add index.ts

--- src/Pages/RequestPage/index.tsx
+++ src/Pages/RequestPage/index.tsx
@@ -1,3 +1,23 @@
+// import * as I from "../../Assets/svg/index";
+import * as S from "./style";
+
 export default function RequestPage() {
-  return <div>RequestPage</div>;
+  return (
+    <S.Container>
+      <S.TextItem>어떤 요청을 보낼까요?</S.TextItem>
+      <S.SubTextItem>요청은 최대 3개까지 보낼 수 있어요!</S.SubTextItem>
+      <S.SelectForm>
+        <S.SelectItem>
+          <S.Icon src="src\Assets\svg\Heart.png" alt="유형사진" />
+          <S.SelectTitle>유형</S.SelectTitle>
+          <S.SelectInfo>같은 유형인 사람들에게 요청을 보내요</S.SelectInfo>
+        </S.SelectItem>
+        <S.SelectItem>
+          <S.Icon src="src\Assets\svg\Hat.png" alt="전공사진" />
+          <S.SelectTitle>전공</S.SelectTitle>
+          <S.SelectInfo>같은 전공인 사람들에게 요청을 보내요</S.SelectInfo>
+        </S.SelectItem>
+      </S.SelectForm>
+    </S.Container>
+  );
 }
