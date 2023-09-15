8e34119b299b067879804a296b5ef7fae0e78ff5
gaoooon <gaon9749@gmail.com>
2023-09-16 00:39:55 +0900

    Update Route

--- src/Pages/RequestPage/index.tsx
+++ src/Pages/RequestPage/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import RequestSelect from "./RequestSelect/index";
 import DetailSelect from "./DetailSelect";
+import RequestWrite from "./RequestWrite";
 
 const RequestPage = () => (
   <Routes>
     <Route path="/" element={<RequestSelect />} />
     <Route path="/:request" element={<DetailSelect />} />
+    <Route path="/write" element={<RequestWrite />} />
   </Routes>
 );
 
