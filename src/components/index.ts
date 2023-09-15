7e8edaea5c0cc814a5a77ed37ae99dc3f494d523
gaoooon <gaon9749@gmail.com>
2023-09-16 00:39:25 +0900

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { default as PrivateRouter } from "./PrivateRouter";
+export { default as GenreRequsetSelect } from "./Request/GenreRequestSelect";
+export { default as MajorRequsetSelect } from "./Request/MajorRequestSelect";
