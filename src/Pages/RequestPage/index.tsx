a52c9323881d7faf29074b3019d5d69ee5f9e34d
gaoooon <gaon9749@gmail.com>
2023-09-17 15:04:06 +0900

    Add Route

--- src/Pages/RequestPage/index.tsx
+++ src/Pages/RequestPage/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import RequestSelect from "./RequestSelect/index";
 import DetailSelect from "./DetailSelect";
 import RequestWrite from "./RequestWrite";
+import RequestCheck from "./RequestCheck";
 
 const RequestPage = () => (
   <Routes>
     <Route path="/" element={<RequestSelect />} />
     <Route path="/:request" element={<DetailSelect />} />
     <Route path="/write" element={<RequestWrite />} />
+    <Route path="/check" element={<RequestCheck />} />
   </Routes>
 );
 
