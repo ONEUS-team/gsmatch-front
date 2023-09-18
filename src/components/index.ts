486649620dfb144cf01bd8cadd377f1282cb198d
gaoooon <gaon9749@gmail.com>
2023-09-18 15:41:19 +0900

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as PrivateRouter } from "./PrivateRouter";
 export { default as GenreRequsetSelect } from "./Request/GenreRequestSelect";
 export { default as MajorRequsetSelect } from "./Request/MajorRequestSelect";
+export { default as RequestSucceed } from "./Request/Result/RequestSucceed";
+export { default as RequestFailed } from "./Request/Result/RequestFailed";
