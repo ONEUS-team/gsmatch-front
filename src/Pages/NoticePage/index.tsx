e41365b1821d0f50eb0eddfcd62427512961e088
신희성 <vvwv3092@gmail.com>
2023-10-12 09:16:10 +0900

    Delete MockData

--- src/Pages/NoticePage/index.tsx
+++ src/Pages/NoticePage/index.tsx
@@ -13,108 +13,14 @@ const filterType: FilterObject = {
   유형: "type",
   전공: "major",
 };
-
-const mokdata = [
-  {
-    id: 1,
-    title: "Sample Title 1",
-    content: "This is the content of sample post 1.",
-    requestType: "type",
-    isOnlyOne: false,
-    author: {
-      id: 101,
-      name: "John Doe",
-      major: "front-end",
-      type: "루피",
-      level: 1,
-      grade: 3,
-    },
-  },
-  {
-    id: 2,
-    title: "Sample Title 2",
-    content: "This is the content of sample post 2.",
-    requestType: "type",
-    isOnlyOne: true,
-    author: {
-      id: 102,
-      name: "Jane Smith",
-      major: "back-end",
-      type: "루피",
-      level: 2,
-      grade: 2,
-    },
-  },
-  {
-    id: 3,
-    title: "Sample Title 3",
-    content: "This is the content of sample post 3.",
-    requestType: "type",
-    isOnlyOne: false,
-    author: {
-      id: 103,
-      name: "Alice Johnson",
-      major: "dev-ops",
-      type: "뽀로로",
-      level: 3,
-      grade: 1,
-    },
-  },
-  {
-    id: 4,
-    title: "Sample Title 4",
-    content: "This is the content of sample post 4.",
-    requestType: "type",
-    isOnlyOne: true,
-    author: {
-      id: 104,
-      name: "Bob Wilson",
-      major: "front-end",
-      type: "루피",
-      level: 4,
-      grade: 2,
-    },
-  },
-  {
-    id: 5,
-    title: "Sample Title 5",
-    content: "This is the content of sample post 5.",
-    requestType: "type",
-    isOnlyOne: false,
-    author: {
-      id: 105,
-      name: "Eva Davis",
-      major: "back-end",
-      type: "뽀로로",
-      level: 5,
-      grade: 1,
-    },
-  },
-  {
-    id: 6,
-    title: "Sample Title 6",
-    content: "This is the content of sample post 6.",
-    requestType: "major",
-    isOnlyOne: true,
-    author: {
-      id: 106,
-      name: "Michael Lee",
-      major: "dev-ops",
-      type: "루피",
-      level: 1,
-      grade: 3,
-    },
-  },
-];
-
 export default function NoticePage() {
   const [range, setRange] = useState<string>("전체");
   const [kind, setKind] = useState<string>("전체");
-  const [datas, setDatas] = useState(mokdata);
+  const [datas, setDatas] = useState([]);
   const [isFilterClick, setIsFilterClick] = useState<boolean>(false);
 
   const dataFilter = useCallback(() => {
-    const newData = mokdata.filter((d) => {
+    const newData = datas.filter((d) => {
       if (range === "전체" && kind === "전체") {
         return d;
       } else if (range === "전체" && kind !== "전체") {
