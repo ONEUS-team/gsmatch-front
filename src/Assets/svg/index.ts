13f7131908aad3c85c9b6656ee36116854849b13
gaoooon <gaon9749@gmail.com>
2023-11-04 06:21:45 +0900

    Update Icon name

--- src/Assets/svg/index.ts
+++ src/Assets/svg/index.ts
@@ -1,7 +1,7 @@
 export { default as ArrowHeadIcon } from "./ArrowHeadIcon";
 export { default as MainPageLogo } from "./MainPageLogo";
 export { default as HeaderLogo } from "./HeaderLogo";
-export { default as PasswordHideIcon } from "./PasswordHideIcon";
+export { default as PasswordBlindIcon } from "./PasswordBlindIcon";
 export { default as PasswordShowIcon } from "./PasswordShowIcon";
 export { default as GenreRequestIcon } from "./GenreRequestIcon";
 export { default as MajorRequestIcon } from "./MajorRequestIcon";
