a3e51007de68c9763e53bd497054d98481081f48
gaoooon <gaon9749@gmail.com>
2023-11-08 14:10:31 +0900

    Add MajorInfo components

---
+++ src/Pages/SignupPage/MajorInfo/style.ts
@@ -0,0 +1,96 @@
+import { Link } from "react-router-dom";
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #111;
+  height: 100vh;
+`;
+
+export const BackContainer = styled.div`
+  width: 36.875rem;
+  height: 2.6875rem;
+  display: flex;
+  align-items: center;
+  column-gap: 1rem;
+  padding: 0.62rem 0.62rem 0.62rem 1.87rem;
+  margin: 6.97rem 0 13.78rem 0;
+`;
+
+export const TextContainer = styled.p`
+  color: #eeeeee;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 120%;
+`;
+
+export const BackLink = styled(Link)`
+  background-color: rgba(0, 0, 0, 0);
+  border: none;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const SelectForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  & > div:not(:last-of-type) {
+    margin-bottom: 2.5rem;
+  }
+`;
+
+export const SelectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+`;
+
+export const SelectBox = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px 8px;
+  width: 500px;
+`;
+
+export const SelectText = styled.p`
+  font-size: 20px;
+  color: #eeeeee;
+  font-weight: 700;
+  margin-bottom: 15px;
+`;
+
+export const SelectButton = styled.button<{ isSelect: boolean }>`
+  border: 1px solid ${({ isSelect }) => (isSelect ? "#F0A0BD" : "#777777")};
+  color: ${({ isSelect }) => (isSelect ? "#F0A0BD" : "#777777")};
+  text-align: center;
+  padding: 0.5rem 1rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120%;
+  border-radius: 1.25rem;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0);
+`;
+
+export const Button = styled.button`
+  display: flex;
+  width: 16.5625rem;
+  height: 3.25rem;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 10rem;
+  background-color: #f3a4b2;
+  border: none;
+  font-weight: 700;
+  font-size: 20px;
+  border-radius: 1.875rem;
+  cursor: pointer;
+`;
