c138990ab238e242fba60eb91aab1b49a6e54fdd
gaoooon <gaon9749@gmail.com>
2023-11-11 18:15:21 +0900

    Add ChatIcon

---
+++ src/Assets/svg/ChatIcon.tsx
@@ -0,0 +1,16 @@
+const ChatIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="30"
+    viewBox="0 0 24 30"
+    fill="none"
+  >
+    <path
+      d="M3.51 7.5375L11.02 11.5625L3.5 10.3125L3.51 7.5375ZM11.01 18.4375L3.5 22.4625V19.6875L11.01 18.4375ZM1.51 3.75L1.5 12.5L16.5 15L1.5 17.5L1.51 26.25L22.5 15L1.51 3.75Z"
+      fill="white"
+    />
+  </svg>
+);
+
+export default ChatIcon;
