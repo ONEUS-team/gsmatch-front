fc9739827dbc0e0e6c1b2bfeef712834a2de722f
gaoooon <gaon9749@gmail.com>
2023-11-17 11:28:10 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,36 +4,31 @@ import PrivateRouter from "./components/PrivateRouter";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path="/signup/*" element={<P.SignupPage />} />
-        <Route path="/login" element={<P.LoginPage />} />
-        <Route path="/main" element={<P.MainPage />} />
-        <Route
-          path="/"
-          element={<PrivateRouter element={<P.ProfilePage />} />}
-        />
-        <Route
-          path="/request/*"
-          element={<PrivateRouter element={<P.RequestPage />} />}
-        />
-        <Route
-          path="/notice"
-          element={<PrivateRouter element={<P.NoticePage />} />}
-        />
-        <Route
-          path="/chat"
-          element={<PrivateRouter element={<P.ChattingPage />} />}
-        />
+    <Routes>
+      <Route path="/signup/*" element={<P.SignupPage />} />
+      <Route path="/login" element={<P.LoginPage />} />
+      <Route path="/main" element={<P.MainPage />} />
+      <Route path="/" element={<PrivateRouter element={<P.ProfilePage />} />} />
+      <Route
+        path="/request/*"
+        element={<PrivateRouter element={<P.RequestPage />} />}
+      />
+      <Route
+        path="/notice"
+        element={<PrivateRouter element={<P.NoticePage />} />}
+      />
+      <Route
+        path="/chat"
+        element={<PrivateRouter element={<P.ChattingPage />} />}
+      />
 
-        <Route
-          path="/survey"
-          element={<PrivateRouter element={<P.SurveyPage />} />}
-        />
+      <Route
+        path="/survey"
+        element={<PrivateRouter element={<P.SurveyPage />} />}
+      />
 
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </>
+      <Route path="*" element={<h1>404 Not Found</h1>} />
+    </Routes>
   );
 }
 
