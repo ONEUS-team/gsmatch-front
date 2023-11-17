02dacaefe8960f4507f3e997e819ee15c3cf8854
gaoooon <gaon9749@gmail.com>
2023-11-17 11:28:34 +0900

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,9 @@
-import React from "react";
 import * as S from "./style";
 import * as I from "../../Assets/svg/index";
 import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const navigate = useNavigate();
-  const params = window.location.pathname;
-
-  console.log(params);
 
   const handleLogoClick = () => navigate("/profile");
 
@@ -22,9 +18,9 @@ const Header = () => {
           <I.HeaderLogo />
         </S.LogoBox>
         <S.RightBox>
-          <S.IconBox onClick={handleNoticeIconClick}>
+          <S.NoticeIconBox onClick={handleNoticeIconClick}>
             <I.NoticeIcon />
-          </S.IconBox>
+          </S.NoticeIconBox>
           <S.IconBox onClick={handleChatIconClick}>
             <I.ChatIcon />
           </S.IconBox>
