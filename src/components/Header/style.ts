e6cf41a8dcfd73ebff242588ba2e3ac1a90b78b2
gaoooon <gaon9749@gmail.com>
2023-11-26 01:26:54 +0900

    Update  style.ts

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 
 export const header = styled.div`
-  width: 100%;
+  width: 100vw;
   background-color: #121212;
   border-bottom: 1px solid #575757;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: absolute;
+  top: 0;
 `;
 
 export const Inner = styled.div`
