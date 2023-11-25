42b5d741effed5a5cec5d220a0c9292fd68c4cdd
gaoooon <gaon9749@gmail.com>
2023-11-26 01:25:40 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import * as P from "./Pages";
 import PrivateRouter from "./components/PrivateRouter";
+import { Header } from "./components";
 
 function App() {
   return (
     <Routes>
+      <Route element={<Header />}>
+        <Route
+          path="/request/*"
+          element={<PrivateRouter element={<P.RequestPage />} />}
+        />
+
+        <Route
+          path="/"
+          element={<PrivateRouter element={<P.ProfilePage />} />}
+        />
+      </Route>
       <Route path="/signup/*" element={<P.SignupPage />} />
       <Route path="/login" element={<P.LoginPage />} />
       <Route path="/main" element={<P.MainPage />} />
-      <Route path="/" element={<PrivateRouter element={<P.ProfilePage />} />} />
-      <Route
-        path="/request/*"
-        element={<PrivateRouter element={<P.RequestPage />} />}
-      />
       <Route
         path="/notice"
         element={<PrivateRouter element={<P.NoticePage />} />}
