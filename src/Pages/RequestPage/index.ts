7d419eb3cb6dae2b24d436bc78894916fd29cc3d
gaoooon <gaon9749@gmail.com>
2023-12-03 04:36:43 +0900

    delete index.ts, router.tsx

--- src/Pages/RequestPage/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as RequestSelect } from "./RequestSelect";
-export { default as DetailSelect } from "./DetailSelect";
-export { default as RequestWrite } from "./RequestWrite";
-export { default as RequestCheck } from "./RequestCheck";
-export { default as RequestResult } from "./RequestResult";
-export { default as RequestDetail } from "./RequestDetail";
