bdfb94e74f1a0204586965f9156b19782ac516be
신희성 <vvwv3092@gmail.com>
2023-12-03 03:20:09 +0900

    Update not use code

--- src/components/PrivateRouter/index.tsx
+++ src/components/PrivateRouter/index.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element }: any) => {
-  // const [isLoggedin, setIsLoggedin] = useState(false);
+  const [isLoggedin, setIsLoggedin] = useState(true);
 
-  return element;
+  useEffect(() => {
+    setIsLoggedin(true);
+  }, []);
+
+  return isLoggedin ? element : <Navigate to="/main"></Navigate>;
 };
 
 export default PrivateRoute;
