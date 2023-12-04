5bf2fe4efbf526734ef3e4ee002a7adcff1a5dbb
gaoooon <gaon9749@gmail.com>
2023-12-05 08:06:53 +0900

    Update user.ts

--- src/types/user.ts
+++ src/types/user.ts
@@ -3,7 +3,7 @@ import { MyRequest } from "./request";
 export interface UserInfo {
   id: number | null;
   username: string | null;
-  grade: string | null;
+  grade: number | null;
   level: number | null;
   gender: string | null;
   type: string | null;
