9f5ce35d6d81bc9ae021b58dabb1326f267e075c
gaoooon <gaon9749@gmail.com>
2023-12-05 08:10:14 +0900

    Update index.tsx

--- src/components/PrivateRouter/index.tsx
+++ src/components/PrivateRouter/index.tsx
@@ -2,9 +2,9 @@ import { ReactNode, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element }: { element: ReactNode }) => {
-  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [isLoggedin, setIsLoggedin] = useState(true);
 
-  setIsLoggedin(true);
+  // setIsLoggedin(true);
 
   return isLoggedin ? element : <Navigate to="/main"></Navigate>;
 };
