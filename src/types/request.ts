865f5e8f7131b0cf55bfa1ecbb58f456372bd718
gaoooon <gaon9749@gmail.com>
2023-12-04 09:56:25 +0900

    Update request.ts

---
+++ src/types/request.ts
@@ -0,0 +1,21 @@
+export type requestType = "major" | "genre";
+export type requestGrade = "1" | "2" | "3" | "모두";
+export type requestGender = "남자" | "여자" | "모두";
+export type requestMajor =
+  | "프론트엔드"
+  | "백엔드"
+  | "디자이너"
+  | "IOS"
+  | "안드로이드"
+  | "DevOps"
+  | "기능반"
+  | "IOT"
+  | "AI";
+
+export interface MyRequest {
+  requestId: number;
+  title: string;
+  content: string;
+  requestType: string;
+  authorName: string;
+}
