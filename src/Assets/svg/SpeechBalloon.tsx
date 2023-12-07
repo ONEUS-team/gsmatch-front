c53c563dd06124f3ef8b4c88ae2c528c9b358d50
gaoooon <gaon9749@gmail.com>
2023-12-07 15:56:59 +0900

    Update build Error

--- src/Assets/svg/SpeechBalloon.tsx
+++ src/Assets/svg/SpeechBalloon.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const SpeechBalloon = () => (
   <svg
     width="107"
--- src/components/PrivateRouter/index.tsx
+++ src/components/PrivateRouter/index.tsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ element }: { element: ReactNode }) => {
   const [isLoggedin, setIsLoggedin] = useState(true);
 
-  // setIsLoggedin(true);
+  setIsLoggedin(true);
 
   return isLoggedin ? element : <Navigate to="/main"></Navigate>;
 };
