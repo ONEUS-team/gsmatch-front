ab0bf5d61d7177a2305c3a5d346714eb8b00e81f
신희성 <vvwv3092@gmail.com>
2023-12-11 09:54:02 +0900

    Remove not used code

--- src/Pages/ProfilePage/index.tsx
+++ src/Pages/ProfilePage/index.tsx
@@ -1,27 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { refresh } from "../../components/api/refresh";
-
 export default function ProfilePage() {
-  const navigate = useNavigate();
-  return (
-    <div>
-      ProfilePage
-      <button
-        onClick={() => {
-          refresh(navigate, null);
-        }}
-      >
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-        adadasdasadadasdas <br />
-      </button>
-    </div>
-  );
+  return <div>ProfilePage</div>;
 }
