7fe6829758c120583b47a6cedbf80de35f43caef
신희성 <vvwv3092@gmail.com>
2023-12-14 21:03:46 +0900

    Add NotFoundPage Component

--- src/components/PrivateRouter/index.tsx
+++ src/components/PrivateRouter/index.tsx
@@ -2,13 +2,7 @@ import { ReactNode, useState } from "react";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ element }: { element: ReactNode }) => {
-  const [isLoggedin, setIsLoggedin] = useState(true);
-
-  if ("login".includes("login")) {
-    setIsLoggedin(true);
-  }
-
-  return isLoggedin ? element : <Navigate to="/main"></Navigate>;
+  return element;
 };
 
 export default PrivateRoute;
