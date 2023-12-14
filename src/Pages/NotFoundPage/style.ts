efe693f136f1362caa8c48685581add42d5f869a
신희성 <vvwv3092@gmail.com>
2023-12-14 23:03:10 +0900

    Add NotFoundPage layout

---
+++ src/Pages/NotFoundPage/style.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #111;
+  height: 100vh;
+`;
+
+export const NotFoundBox = styled.div`
+  text-align: left;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+export const NotFoundTitle = styled.h1`
+  color: white;
+  font-family: SEBANG Gothic;
+  font-size: 72px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: 3.6px;
+`;
+
+export const HighLight = styled.span`
+  color: #f3a4b2;
+`;
+
+export const NotFoundSubTitle = styled.h2`
+  color: white;
+  font-family: Pretendard;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120%;
+`;
+
+export const NotFoundContent = styled.h2`
+  color: white;
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 120%;
+  width: 400px;
+`;
