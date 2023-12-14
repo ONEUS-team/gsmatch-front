08af0562a6bf04e0355baab7f6532ba46f613cee
gaoooon <gaon9749@gmail.com>
2023-12-14 23:51:51 +0900

    Add PartnerMessageCard

---
+++ src/components/MessageCard/PartnerMessageCard/index.tsx
@@ -0,0 +1,29 @@
+import * as S from "./style";
+
+interface Props {
+  chat: {
+    sender: {
+      id: number;
+      senderId: string;
+      senderName: string;
+    };
+    message: string;
+    sendDate: string;
+  };
+  partnerType: string;
+}
+
+const PartnerMessageCard: React.FC<Props> = ({ chat, partnerType }) => {
+  const src = partnerType;
+  return (
+    <S.LineContainer>
+      <S.MessageContainer>
+        <S.TypeImg src={src} />
+        <S.Message>{chat.message}</S.Message>
+        <S.TimeCard>오후 9 : 21</S.TimeCard>
+      </S.MessageContainer>
+    </S.LineContainer>
+  );
+};
+
+export default PartnerMessageCard;
