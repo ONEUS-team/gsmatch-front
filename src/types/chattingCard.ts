71132724bdf751d62e183ac99058d8205ad1b345
gaoooon <gaon9749@gmail.com>
2023-12-14 20:24:42 +0900

    Add chattingCard.ts

--- src/types/chattingCard.ts
+++ src/types/chattingCard.ts
@@ -1,13 +1,11 @@
-import { CharacterType, Grade, Major } from "./utilType";
-
 export interface ChattingCard {
   id: number;
   roomName: string;
   partner: {
     id: number;
     name: string;
-    major: Major;
-    grade: Grade;
-    type: CharacterType;
+    major: string;
+    grade: string;
+    type: string;
   };
 }
