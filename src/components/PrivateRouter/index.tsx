3a40a5eadaa432cb452f567e75f4be06d2717ea5
신희성 <vvwv3092@gmail.com>
2023-12-14 23:11:13 +0900

    Update private router

--- src/components/PrivateRouter/index.tsx
+++ src/components/PrivateRouter/index.tsx
@@ -1,6 +1,13 @@
 import { ReactNode } from "react";
 
 const PrivateRoute = ({ element }: { element: ReactNode }) => {
+  // const [isLoggedin, setIsLoggedin] = useState(true);
+
+  // if ("login".includes("login")) {
+  //   setIsLoggedin(true);
+  // }
+
+  // return isLoggedin ? element : <Navigate to="/main"></Navigate>;
   return element;
 };
 
