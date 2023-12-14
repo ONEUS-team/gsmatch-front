1e55543ddcd00acca98e1fc8dd74a991666f9f59
gaoooon <gaon9749@gmail.com>
2023-12-14 23:51:19 +0900

    Update index.tsx

--- src/Pages/ChattingPage/ChattingBasic/index.tsx
+++ src/Pages/ChattingPage/ChattingBasic/index.tsx
@@ -3,33 +3,36 @@ import * as S from "./style";
 
 const data = [
   {
-    message: "뭐",
-    roomId: "1",
-    sendDate: "2023-12-09T14:55:18.311214129",
-    sender: {
-      senderName: "신희성",
-      senderId: "1",
+    id: 1,
+    roomName: "아니 왜 손흥민...",
+    partner: {
+      id: 1110,
+      name: "황희찬",
+      major: "FRONT",
+      grade: "ONE",
       type: "PORORO",
     },
   },
   {
-    message: "뭐",
-    roomId: "5",
-    sendDate: "2023-12-09T14:55:18.311214129",
-    sender: {
-      senderName: "신희성",
-      senderId: "1",
-      type: "PORORO",
+    id: 2,
+    roomName: "아니 왜 손흥민...",
+    partner: {
+      id: 1210,
+      name: "황희찬",
+      major: "FRONT",
+      grade: "TWO",
+      type: "EDI",
     },
   },
   {
-    message: "뭐",
-    roomId: "123",
-    sendDate: "2023-12-09T14:55:18.311214129",
-    sender: {
-      senderName: "신희성",
-      senderId: "1",
-      type: "PORORO",
+    id: 3,
+    roomName: "아니 왜 손흥민123456789",
+    partner: {
+      id: 1130,
+      name: "황희찬",
+      major: "FRONT",
+      grade: "ONE",
+      type: "POBI",
     },
   },
 ];
@@ -37,7 +40,7 @@ const data = [
 const ChattingBasic = () => {
   return (
     <S.Container>
-      <ChattingCardList chatList={data} />
+      <ChattingCardList cardList={data} />
       <S.NotSelectMessageContainer>
         <S.TextBox>
           <img src="../../../src/Assets/png/NotSelectMessage.png" alt="" />
