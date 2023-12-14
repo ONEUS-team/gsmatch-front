37d1d2b60a70ad2e6eee0bfb766a1d41516bf1c3
gaoooon <gaon9749@gmail.com>
2023-12-14 17:54:05 +0900

    Add convert.ts

---
+++ src/types/convert.ts
@@ -0,0 +1,29 @@
+export interface GradeConvert {
+  ONE: "1";
+  TWO: "2";
+  THREE: "3";
+}
+
+export interface GenderConvert {
+  MALE: "남자";
+  FEMALE: "여자";
+}
+
+export interface MajorConvert {
+  FRONT: "프론트엔드";
+  BACK: "백엔드";
+  DESIGN: "디자인";
+  IOS: "IOS";
+  AOS: "안드로이드";
+  DEVOPS: "DevOps";
+  WORLD_SKILL: "기능반";
+  IOT: "IOT";
+  AI: "AI";
+}
+
+export interface TypeConvert {
+  PORORO: "뽀로로";
+  LUPI: "루피";
+  POBI: "포비";
+  EDI: "에디";
+}
