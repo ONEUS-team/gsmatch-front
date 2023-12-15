4fbd0061aedb5af79071e1399ba326adee87c1e3
gaoooon <gaon9749@gmail.com>
2023-12-15 11:15:44 +0900

    Add PencilIcon

--- src/Assets/svg/index.ts
+++ src/Assets/svg/index.ts
@@ -26,3 +26,4 @@ export { default as MoreShowButton } from "./MoreShowButton";
 export { default as SendIcon } from "./SendIcon";
 export { default as ThumbMessageIcon } from "./ThumbMessageIcon";
 export { default as SpeechBalloon } from "./SpeechBalloon";
+export { default as PencilIcon } from "./PencilIcon";
