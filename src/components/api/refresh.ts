25eaf1cf08a9f1ed8479d9ddd8893adf377dcb9b
신희성 <vvwv3092@gmail.com>
2023-12-16 02:55:06 +0900

    Update refresh

--- src/components/api/refresh.ts
+++ src/components/api/refresh.ts
@@ -10,15 +10,15 @@ export const refresh = (
       "https://port-0-gsmatch-back-f02w2almh8gdgs.sel5.cloudtype.app/refresh",
       {
         headers: {
-          Authorization: `Bearer${localStorage.getItem("Authorization")}`,
-          "Refresh-Token": `Bearer${localStorage.getItem("Refresh-Token")}`,
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+          "Refresh-Token": `Bearer ${localStorage.getItem("refreshToken")}`,
         },
         withCredentials: true,
       }
     )
     .then((response) => {
       localStorage.setItem(
-        "Access-Token",
+        "accessToken",
         response.headers.authorization.replace("Bearer", "")
       );
       if (callBack != null) callBack();
