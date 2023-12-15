1013a28dcaa51f771a5fa29428eeb17bfcad7560
gaoooon <gaon9749@gmail.com>
2023-12-15 23:14:17 +0900

    Add axiosInstace.ts

--- src/libs/api/axiosInstance.ts
+++ src/libs/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://port-0-gsmatch-back-57lz2alpp1ckys.sel4.cloudtype.app",
+  baseURL: "https://port-0-gsmatch-back-f02w2almh8gdgs.sel5.cloudtype.app",
 });
 
 export default axiosInstance;
