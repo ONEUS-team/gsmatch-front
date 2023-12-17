74ad4a4ff2b3efdc492aab14301b188aede38c2d
gaoooon <gaon9749@gmail.com>
2023-12-18 01:10:03 +0900

    Update request.ts

--- src/types/request.ts
+++ src/types/request.ts
@@ -1,3 +1,5 @@
+import { CharacterType, Gender, Grade, Major, RequestType } from "./utilType";
+
 export type requestType = "major" | "genre";
 export type requestGrade = "1" | "2" | "3" | "모두";
 export type requestGender = "남자" | "여자" | "모두";
@@ -19,4 +21,32 @@ export interface MyRequest {
   requestType: string;
   authorName: string;
   image: string;
+}
+
+export interface DetailType {
+  id: number;
+  title: string;
+  content: string;
+  imageNames: string[];
+  requestType: RequestType;
+  isOnlyOne: boolean;
+  author: {
+    id: number;
+    name: string;
+    major: Major;
+    type: CharacterType;
+    level: number;
+    grade: Grade;
+  };
+}
+
+export interface MyInfo {
+  id: number;
+  username: string;
+  grade: Grade;
+  level: number;
+  gender: Gender;
+  type: CharacterType;
+  point: number;
+  major: Major;
 }
