70fa64b07041e68c9d76ff8a3e4c0d99bf967f04
gaoooon <gaon9749@gmail.com>
2023-12-18 10:16:21 +0900

    Add navigate

--- src/components/Request/Result/RequestSucceed.tsx
+++ src/components/Request/Result/RequestSucceed.tsx
@@ -1,11 +1,14 @@
 import * as S from "./style";
 import * as I from "../../../Assets/svg/index";
+import { useNavigate } from "react-router-dom";
 
 const RequestSucceed = () => {
+  const navigate = useNavigate();
+
   return (
     <S.Container>
       <S.TextItem>요청이 성공적으로 보내졌어요! </S.TextItem>
-      <S.SucceedButton>
+      <S.SucceedButton onClick={() => navigate("/")}>
         나가기
         <I.ArrowButtonIcon />
       </S.SucceedButton>
