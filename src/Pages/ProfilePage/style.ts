bdbfbac31517c5d84ff627cba6e6a0ed0c22c51a
gaoooon <gaon9749@gmail.com>
2023-12-19 14:59:36 +0900

    Update style.ts

--- src/Pages/ProfilePage/style.ts
+++ src/Pages/ProfilePage/style.ts
@@ -11,6 +11,17 @@ export const Container = styled.div<{ isScrollable: boolean }>`
   overflow-y: auto;
 `;
 
+export const TypeImgBox = styled.div`
+  position: relative;
+`;
+
+export const LevelEdge = styled.img`
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0);
+  top: -2.95rem;
+  left: -3.12rem;
+`;
+
 export const ProfileBox = styled.div`
   margin-top: 8.435rem;
   display: flex;
@@ -30,6 +41,7 @@ export const InfoBox = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 0.75rem;
+  align-items: center;
 `;
 
 export const TopInfo = styled.p`
@@ -47,6 +59,10 @@ export const BottomInfo = styled.p`
   font-style: normal;
   font-weight: 700;
   line-height: 120%;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.75rem;
 `;
 
 export const BannerContainer = styled.div`
@@ -116,4 +132,39 @@ export const TypeBannerText = styled(RequestBannerText)`
     content: "?";
     color: #f0a0bd;
   }
+`;
+
+export const PointBar = styled.div<{ level: number; point: number }>`
+  border-radius: 50px;
+  width: 15.0625rem;
+  height: 0.5rem;
+  background: linear-gradient(
+    90deg,
+    #f3a4b2 0%,
+    #f3a4b2
+      ${({ level, point }) =>
+        " " +
+        (level === 1
+          ? (point / 20) * 100 + "%"
+          : level === 2
+          ? ((point - 20) / 30) * 100 + "%"
+          : level === 3
+          ? ((point - 50) / 30) * 100 + "%"
+          : level === 4
+          ? ((point - 80) / 40) * 100 + "%"
+          : "100%")},
+    #eee
+      ${({ level, point }) =>
+        " " +
+        (level === 1
+          ? (point / 20) * 100 + "%"
+          : level === 2
+          ? ((point - 20) / 30) * 100 + "%"
+          : level === 3
+          ? ((point - 50) / 30) * 100 + "%"
+          : level === 4
+          ? ((point - 80) / 40) * 100 + "%"
+          : "100%")},
+    #eee 100%
+  );
 `;
