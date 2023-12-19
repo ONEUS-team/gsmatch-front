cf6f02e2600b59eb8056e60fe58d0833c0d92817
gaoooon <gaon9749@gmail.com>
2023-12-19 15:17:57 +0900

    Update index.tsx

--- src/Pages/SignupPage/MajorInfo/index.tsx
+++ src/Pages/SignupPage/MajorInfo/index.tsx
@@ -7,6 +7,7 @@ import {
   GenderConvert,
   MajorConvert,
 } from "../../../types/convert";
+import axiosInstance from "../../../libs/api/axiosInstance";
 
 interface Props {
   signupName: string;
@@ -20,18 +21,15 @@ interface Props {
   setSignupGender: React.Dispatch<React.SetStateAction<string>>;
   setSignupMajor: React.Dispatch<React.SetStateAction<string>>;
 }
-
 const gradeConvert: GradeConvert = {
   ONE: "1",
   TWO: "2",
   THREE: "3",
 };
-
 const genderConvert: GenderConvert = {
   MALE: "남자",
   FEMALE: "여자",
 };
-
 const majorConvert: MajorConvert = {
   FRONT: "프론트엔드",
   BACK: "백엔드",
@@ -43,7 +41,6 @@ const majorConvert: MajorConvert = {
   IOT: "IOT",
   AI: "AI",
 };
-
 const MajorInfo: React.FC<Props> = ({
   signupName,
   signupEmail,
@@ -57,21 +54,36 @@ const MajorInfo: React.FC<Props> = ({
   setSignupMajor,
 }) => {
   const navigate = useNavigate();
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const emailRegex = /^[sS][0-9]{5}@gsm\.hs\.kr$/;
   const passwordRegex =
     /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@#$!%^&*])[A-Za-z0-9@#$!%^&*]{8,20}$/;
+
+  const signup = async (body: {
+    username: string;
+    password: string;
+    email: string;
+    grade: string;
+    gender: string;
+    major: string;
+  }) => {
+    try {
+      await axiosInstance.post("/api/auth/signup", body);
+      navigate("/login");
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const [grades, setGrades] = useState([
     { grade: "ONE", isSelect: false },
     { grade: "TWO", isSelect: false },
     { grade: "THREE", isSelect: false },
   ]);
-
   const [genders, setGenders] = useState([
     { gender: "MALE", isSelect: false },
     { gender: "FEMALE", isSelect: false },
   ]);
-
   const [majors, setMajors] = useState([
     { major: "FRONT", isSelect: false },
     { major: "BACK", isSelect: false },
@@ -83,43 +95,47 @@ const MajorInfo: React.FC<Props> = ({
     { major: "IOT", isSelect: false },
     { major: "AI", isSelect: false },
   ]);
-
   const changeGrade = (e: React.MouseEvent<HTMLButtonElement>) => {
     const ID: string = e.currentTarget.id;
-
     const newGrades = grades.map((g) =>
       g.grade === ID ? { ...g, isSelect: true } : { ...g, isSelect: false }
     );
     setSignupGrade(ID);
     setGrades(newGrades);
   };
-
   const changeGender = (e: React.MouseEvent<HTMLButtonElement>) => {
     const ID: string = e.currentTarget.id;
-
     const newGenders = genders.map((g) =>
       g.gender === ID ? { ...g, isSelect: true } : { ...g, isSelect: false }
     );
-
     setSignupGender(ID);
     setGenders(newGenders);
   };
-
   const changeMajor = (e: React.MouseEvent<HTMLButtonElement>) => {
     const ID: string = e.currentTarget.id;
-
     const newMajors = majors.map((m) =>
       m.major === ID ? { ...m, isSelect: true } : { ...m, isSelect: false }
     );
-
     setSignupMajor(ID);
     setMajors(newMajors);
   };
 
-  const goToNextPage = () => {
-    if (signupGender !== null && signupGrade !== null && signupMajor !== null)
-      navigate("/signup/resultx");
-    else alert("실패");
+  const goToNextPage = async () => {
+    if (signupGender !== null && signupGrade !== null && signupMajor !== null) {
+      setIsDisabled(true);
+
+      await signup({
+        username: signupName,
+        password: signupPassword,
+        email: signupEmail,
+        grade: signupGrade,
+        gender: signupGender,
+        major: signupMajor,
+      });
+
+      setIsDisabled(false);
+      navigate("/signup/result");
+    } else alert("실패");
   };
 
   const checkGeneralInfo = () => {
@@ -128,20 +144,16 @@ const MajorInfo: React.FC<Props> = ({
     const passwordMatch =
       signupPassword.length > 8 && passwordRegex.test(signupPassword);
     const checkPasswordMatch = signupCheckPassword == signupPassword;
-
     const isMatch =
       nameMatch && emailMatch && passwordMatch && checkPasswordMatch;
-
     if (!isMatch) {
       navigate("/signup");
     }
   };
-
   useEffect(() => {
     checkGeneralInfo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
   return (
     <S.Container>
       <S.BackContainer>
@@ -197,9 +209,10 @@ const MajorInfo: React.FC<Props> = ({
           </S.SelectBox>
         </S.SelectContainer>
       </S.SelectForm>
-      <S.Button onClick={goToNextPage}>다음</S.Button>
+      <S.Button disabled={isDisabled} onClick={goToNextPage}>
+        다음
+      </S.Button>
     </S.Container>
   );
 };
-
 export default MajorInfo;
