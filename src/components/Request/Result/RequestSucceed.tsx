6f4b567c1e7d2d6bac9544e78abf3e2e08818d20
신희성 <vvwv3092@gmail.com>
2023-12-19 10:30:14 +0900

    BuildError request img

--- src/components/Request/Result/RequestSucceed.tsx
+++ src/components/Request/Result/RequestSucceed.tsx
@@ -2,12 +2,14 @@ import * as S from "./style";
 import * as I from "../../../Assets/svg/index";
 import { useNavigate } from "react-router-dom";
 
+import Succeed from "../../../Assets/png/succeedImg.png";
+
 const RequestSucceed = () => {
   const navigate = useNavigate();
 
   return (
     <S.Container>
-      <S.Img src="../../src/Assets/png/succeedimg.png" />
+      <S.Img src={Succeed} />
       <S.TextItem>요청이 성공적으로 보내졌어요! </S.TextItem>
       <S.SucceedButton onClick={() => navigate("/")}>
         나가기
