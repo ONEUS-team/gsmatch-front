995744cbc63ab6d1169a93a01f5987b744a61dc7
신희성 <vvwv3092@gmail.com>
2023-12-19 11:24:18 +0900

    BuildError profile request defualt img

--- src/components/RequestCard/index.tsx
+++ src/components/RequestCard/index.tsx
@@ -2,6 +2,8 @@ import { MyRequest } from "../../types/request";
 import * as S from "./style";
 import { MacbookIcon, PeopleIcon } from "../../Assets/svg";
 
+import requestCardDefault from "../../Assets/png/requestCardDefault.png";
+
 interface Props {
   request: MyRequest;
 }
@@ -10,7 +12,7 @@ const RequestCard: React.FC<Props> = ({ request }) => {
   const type = request.requestType === "TYPE" ? "유형" : "전공";
   const src = request.image
     ? `https://port-0-gsmatch-back-f02w2almh8gdgs.sel5.cloudtype.app/api${request.image}`
-    : "src/Assets/png/requestCardDefault.png";
+    : requestCardDefault;
 
   return (
     <S.Card to={`/request/detail/${request.requestId}`}>
