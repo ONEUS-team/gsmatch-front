a97572c964e293d5b469b42f91241f82ea61ab07
신희성 <vvwv3092@gmail.com>
2023-12-21 17:42:54 +0900

    Fixed img error

--- src/components/MessageCard/PartnerMessageCard/index.tsx
+++ src/components/MessageCard/PartnerMessageCard/index.tsx
@@ -1,6 +1,11 @@
 import { IChatData } from "../../../Pages/ChattingPage/ChattingRoom";
 import * as S from "./style";
 
+import PORORO from "../../../Assets/png/pororo.png";
+import POBI from "../../../Assets/png/pobi.png";
+import LUPI from "../../../Assets/png/LUPI.png";
+import EDI from "../../../Assets/png/edi.png";
+
 interface Props {
   chat: IChatData;
   partnerType: string;
@@ -12,12 +17,21 @@ const PartnerMessageCard: React.FC<Props> = ({
   partnerType,
   sendDate,
 }) => {
+  const profileSrc =
+    partnerType == "PORORO"
+      ? PORORO
+      : partnerType == "POBI"
+      ? POBI
+      : partnerType == "LUPI"
+      ? LUPI
+      : partnerType == "EDI"
+      ? EDI
+      : "";
+
   return (
     <S.LineContainer>
       <S.MessageContainer>
-        {partnerType !== null && (
-          <S.TypeImg src={`../../src/Assets/png/${partnerType}.png`} />
-        )}
+        {partnerType !== null && <S.TypeImg src={profileSrc} />}
         <S.Message>{chat.message}</S.Message>
         <S.TimeCard>{sendDate}</S.TimeCard>
       </S.MessageContainer>
